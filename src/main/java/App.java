b2a5f05199febfd8d534f4c5065f522953401ade
Joaquin Scarone <joaquinscarone1012@gmail.com>
2026-08-11 20:52:37 -0300

    science news funcionando

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
-import Api.HttpService;
 import Calculator.Tokenizer;
+import NewsApi.HttpService;
 
 public class App {
     public static void main(String[] args) throws Exception {
         HttpService httpService= new HttpService();
-        httpService.getArticle();
+        httpService.showArticle();
     }
 }
---
+++ src/main/java/NewsApi/ArticleResponse.java
@@ -0,0 +1,66 @@
+package NewsApi;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ArticleResponse{
+    @Override
+    public String toString() {
+        return "ArticleResponse [status=" + status + ", totalResults=" + totalResults + ", articles=" + articles + "]";
+    }
+
+    private String status;
+    private int totalResults;
+    private List<Article> articles;
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getTotalResults() {
+        return totalResults;
+    }
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+    public List<Article> getArticles() {
+        return articles;
+    }
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Article{
+        @Override
+        public String toString() {
+            return "Article [title=" + title + ", description="+ description + ", url=" + url + "]";
+        }
+        private String title;
+        private String description;
+        private String url;
+
+        public String getTitle() {
+            return title;
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+        public String getUrl() {
+            return url;
+        }
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+}
